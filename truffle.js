15e539779cd90c7793cb1941c0627033873a7a57
Dominic Williams <dominicwilliams007@gmail.com>
2016-11-29 18:30:38 -0800

    Fettled reporting of balance too small to forward plus fix

--- truffle.js
+++ truffle.js
@@ -11,8 +11,29 @@ module.exports = {
     "images/": "images/",
     "deps/": "deps/"
   },
-  rpc: {
+  networks: {
+  "live": {
+    network_id: 1, // Ethereum public network
+    // optional config values
+    // host - defaults to "localhost"
+    // port - defaults to 8545
+    // gas
+    // gasPrice
+    // from - default address to use for any transaction Truffle makes during migrations
+  },
+  "morden": {
+    network_id: 2,        // Official Ethereum test network
+    host: "178.25.19.88", // Random IP for example purposes (do not use)
+    port: 80
+  },
+  "staging": {
+    network_id: 1337 // custom private network
+    // use default rpc settings
+  },
+  "development": {
+    network_id: "default",
     host: "localhost",
-    port: 8545
+    port: 8545    
   }
+}
 };
