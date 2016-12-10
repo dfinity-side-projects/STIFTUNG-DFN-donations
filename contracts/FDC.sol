28f5d1c8b7094351803171d8c1375f3cdf62d2e6
Timo Hanke <timo.t.hanke@gmail.com>
2016-12-09 19:32:51 -0800

    Fix call to setCap in FDC constructor Was using state number instead of phase number

--- contracts/FDC.sol
+++ contracts/FDC.sol
@@ -127,8 +127,8 @@ contract FDC is TokenTracker, Phased, StepFunction, Caps, Parameters {
     setMaxDelay(3, maxDelay);
 
     // initialize Caps base contract
-    setCap(uint(state.donPhase0), phase0Cap);
-    setCap(uint(state.donPhase1), phase1Cap);
+    setCap(2, phase0Cap);
+    setCap(4, phase1Cap);
   }
   
   //
