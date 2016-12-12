828d1a8a5f3afc4c210d8449704660152b2c3cd2
Timo Hanke <timo.t.hanke@gmail.com>
2016-12-12 12:59:26 -0800

    Fix doc string problem again

--- contracts/Parameters.sol
+++ contracts/Parameters.sol
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-/**
+/*
  * @title:  Configuration parameters for the FDC
  * @author: Timo Hanke <timo.t.hanke@gmail.com> 
  */
