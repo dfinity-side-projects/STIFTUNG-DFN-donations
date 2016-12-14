01593143e5c01dfdc5f60325a609db566e8d2a1e
Timo Hanke <timo.t.hanke@gmail.com>
2016-12-13 22:33:07 -0800

    Setting final multiplier values

--- contracts/Parameters.sol
+++ contracts/Parameters.sol
@@ -35,13 +35,13 @@ contract Parameters {
    * Time Constants
    *
    * Phases are, in this order: 
-   *  earlyContribution: defined by end time
+   *  earlyContribution (defined by end time)
    *  pause
-   *  donation phase0: defined by start and end time
+   *  donation phase0 (defined by start and end time)
    *  pause
-   *  donation phase1: defined by start and end time
+   *  donation phase1 (defined by start and end time)
    *  pause
-   *  finalization: defined by start time, ends manually
+   *  finalization (defined by start time, ends manually)
    *  done
    */
 
@@ -49,29 +49,32 @@ contract Parameters {
   // TZ="Europe/Zurich" date -d "2016-12-19 19:00" "+%s"
   uint public constant phase0StartTime      = 1482170400; 
   
-  // The other phase transitions are defined by offsets from the start of phase 0
-  uint public constant earlyContribEndTime  = phase0StartTime - 1 days; 
-  uint public constant phase0EndTime        = phase0StartTime + 6 weeks;
-  
   // The start of phase 1 is set to 2017-03-13 19:00 of timezone Europe/Zurich
   // TZ="Europe/Zurich" date -d "2017-03-13 19:00" "+%s"
   uint public constant phase1StartTime      = 1489428000; 
+  
+  // Phase transitions defined by offsets from the start of phase 0 and 1
+  uint public constant earlyContribEndTime  = phase0StartTime - 1 days; 
+  uint public constant phase0EndTime        = phase0StartTime + 6 weeks;
   uint public constant phase1EndTime        = phase1StartTime + 6 weeks;
   uint public constant finalizeStartTime    = phase1EndTime   + 1 weeks;
   
   // The finalization phase has a dummy end time because it is ended manually
   uint public constant finalizeEndTime      = finalizeStartTime + 1000 years;
   
-  // The maximum time by which donation phase 1 can be delayed from the start time defined above
+  // The maximum time by which donation phase 1 can be delayed from the start 
+  // time defined above
   uint public constant donPhase1MaxDelay    = 180 days;
 
-  // The time for which donation phases remain open after they reach their respective targets   
+  // The time for which donation phases remain open after they reach their 
+  // respective targets   
   uint public constant gracePeriodAfterTarget  = 1 hours;
 
   /*
    * Token issuance
    * 
-   * The following configuration parameters completely govern all aspects of the token issuance.
+   * The following configuration parameters completely govern all aspects of the 
+   * token issuance.
    */
   
   // Tokens assigned for the equivalent of 1 CHF in donations
@@ -80,11 +83,11 @@ contract Parameters {
   // Minimal donation amount for a single on-chain donation
   uint public constant minDonation = 1 ether; 
  
-  // Bonus in percent applied to phase 0 donations 
-  uint public constant phase0Bonus = 50; // multiplier in % applied to all donations during donation phase 0
+  // Bonus in percent added to donations throughout donation phase 0 
+  uint public constant phase0Bonus = 150; 
   
-  // Bonus in percent at beginning of donation phase 1  
-  uint public constant phase1InitialBonus = 40;
+  // Bonus in percent added to donations at beginning of donation phase 1  
+  uint public constant phase1InitialBonus = 25;
   
   // Number of down-steps for the bonus during donation phase 1
   uint public constant phase1BonusSteps = 5;
@@ -94,6 +97,7 @@ contract Parameters {
   uint public constant phase0Target = 1 * millionInCents; 
   uint public constant phase1Target = 20 * millionInCents;
 
-  // Share of tokens eventually assigned to early contributors in % of all tokens eventually in existence
+  // Share of tokens eventually assigned to DFINITY Stiftung and early 
+  // contributors in % of all tokens eventually in existence
   uint public constant earlyContribShare = 20; 
 }
