ad8dee11f912944d082c5e32a41bf5930cb7acdd
Tom Ding <tom@string.technology>
2016-12-14 17:14:21 -0800

    finally made the live network deployment work

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,39 +1,47 @@
 module.exports = function(deployer, network) {
+    if (network != "live") {
+        var accounts = web3.eth.accounts;
+        var walletAddr = accounts[0];
+        var registrar = accounts[1];
+        var exchangeRateAddr = accounts[2];
+        if (network == "ropsten") {
+            walletAddr = registrar = exchangeRateAddr = "0x8C482C7793c98a01B0bf03b332436f53325A7d2B";
+        } else if (network == "live") {
+            walletAddr = registrar = exchangeRateAddr = "0x0a6e23d3a9d6a1ed31f4791614bbc44c04930c66";
+        }
+        //
+        // Deploy FDC using the standard accounts provided by testRPC for testing
+        // purposes. These can be accessed from any test unit :)
+        //
+        deployer.deploy(FDC, walletAddr, registrar, exchangeRateAddr).then(function() {
+            // deployed OK!
+            var fdc = FDC.deployed();
+            console.log("FDC deployed at "+fdc.address+"\nUsing controllers:\n"+
+                " foundationWalletAddr =    "+walletAddr+"\n donationRegistrarAddr =   "+
+                registrar+"\n exchangeRateUpdaterAddr = "+ exchangeRateAddr);
+            // configure initial exchange rate necessary for operation
 
-  if (network != "live") {
-    //
-    // Deploy FDC using the standard accounts provided by testRPC for testing
-    // purposes. These can be accessed from any test unit :)
-    //
-    var accounts = web3.eth.accounts;
-    deployer.deploy(FDC, accounts[0], accounts[1], accounts[2]).then(function() {
-      // deployed OK!
-      var fdc = FDC.deployed();
-      console.log("FDC deployed at "+fdc.address+"\nUsing controllers:\n"+
-        " foundationWalletAddr =    "+accounts[0]+"\n donationRegistrarAddr =   "+
-        accounts[1]+"\n exchangeRateUpdaterAddr = "+accounts[2]);
-      // configure initial exchange rate necessary for operation
+            /*fdc.setWeiPerCHF(web3.toWei('0.125', 'ether'), {gas:300000, from: exchangeRateAddr}).then(function(txID) {
+                console.log("Configured initial exchange rate for new FDC");
+            }).catch(function(e) {
+                console.log("Exception setting initial exchange rate in FDC: "+e);
+                throw e;
+            });
+            */
 
-      fdc.setWeiPerCHF(web3.toWei('0.125', 'ether'), {gas:300000, from: accounts[2]}).then(function(txID) {
-        console.log("Configured initial exchange rate for new FDC");
-      }).catch(function(e) {
-        console.log("Exception setting initial exchange rate in FDC: "+e);
-        throw e;
-      });
+        }).catch(function(e) {
+            console.log("Error deploying FDC");
+        });
+    } else {
+        //
+        // TODO add identities of multi-sig controllers for production!!
+        //
+        var foundationWalletAddr = "0x733DcDb9a7C60067d65eC731C49e95eD995e59fD";
+        // Identity of dummy registrar
+        var donationRegistrarAddr = "0x733DcDb9a7C60067d65eC731C49e95eD995e59fD";
+        // Identity of dummy exchange rate updater
+        var exchangeRateUpdaterAddr = "0x733DcDb9a7C60067d65eC731C49e95eD995e59fD";
 
-    }).catch(function(e) {
-      console.log("Error deploying FDC");
-    });
-  } else {
-    //
-    // TODO add identities of multi-sig controllers for production!!
-    //
-    var foundationWalletAddr = "...";
-    // Identity of dummy registrar
-    var donationRegistrarAddr = "...";
-    // Identity of dummy exchange rate updater
-    var exchangeRateUpdaterAddr = "...";
-
-    deployer.deploy(FDC, foundationWalletAddr, donationRegistrarAddr, exchangeRateUpdaterAddr);
-  }
+        deployer.deploy(FDC, foundationWalletAddr, donationRegistrarAddr, exchangeRateUpdaterAddr);
+    }
 };
