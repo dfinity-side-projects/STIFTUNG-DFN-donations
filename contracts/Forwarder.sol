ec0d59b9981227ea1bf29511bb4bd15974aaef24
Timo Hanke <timo.t.hanke@gmail.com>
2017-01-10 12:23:34 -0800

    Fix non-payable functions in{Wallet,Forwarder}.sol

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -8,9 +8,6 @@ contract Forwarder {
   bytes4 public checksum;
 
   function Forwarder(address fdcAddr, address _donorAddr) {
-    // The forwarder should not have positive balance
-    if (msg.value > 0) { throw; }
-    
     fdc = FDC(fdcAddr);
     donorAddr = _donorAddr;
     checksum = bytes4(sha256(_donorAddr));
