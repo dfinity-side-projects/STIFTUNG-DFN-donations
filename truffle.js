b9227cae817e0204dfc7da44170d750770258bd2
Enzo Haussecker <enzo@sovereign.io>
2017-01-13 21:19:12 -0800

    Rollback to previous Truffle configuration

--- truffle.js
+++ truffle.js
@@ -15,26 +15,34 @@ module.exports = {
     "deps/": "deps/"
   },
   networks: {
-    "main": {
-      network_id: "default",
-      host: "parity.dfinity.build",
-      port: 80,
+  "live": {
+    network_id: "default", // Ethereum public network
+    host: "54.201.221.105",
+    port: 8545,
       gas: 3000000,
       gasPrice: 20000000000,
-      from: "0xcf49327643ac9ee9188f6c74d08f35bf91bce055"
+      from: "cf49327643ac9ee9188f6c74d08f35bf91bce055"
+  },
+  "morden": {
+    network_id: 2,        // Official Ethereum test network
+    host: "178.25.19.88", // Random IP for example purposes (do not use)
+    port: 80
+  },
+    "ropsten": {
+      network_id:3,
+      host:"127.0.0.1",
+      gas: 4000000,
+      from: "0x882ED121Ea15230251af7d84C595A4864b3BBCCc",
+      port:18545
     },
-    "test": {
-      network_id: 2,
-      host: "test.parity.dfinity.build",
-      port: 80,
-      gas: 3000000,
-      gasPrice: 20000000000,
-      from: "0x882ED121Ea15230251af7d84C595A4864b3BBCCc"
-    },
-    "development": {
-      network_id: 3,
-      host: "localhost",
-      port: 7000
-    }
+  "staging": {
+    network_id: 1337 // custom private network
+    // use default rpc settings
+  },
+  "development": {
+    network_id: "dev",
+    host: "localhost",
+    port: 7000
   }
+}
 };
