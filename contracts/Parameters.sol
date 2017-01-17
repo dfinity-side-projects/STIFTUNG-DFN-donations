94f8256356bc97e97c24a18ce025141ccbec32a0
developer <developer@dfinity.dfinity.lan>
2017-01-17 23:45:14 +0000

    Revise documentation

--- contracts/Parameters.sol
+++ contracts/Parameters.sol
@@ -45,7 +45,7 @@ contract Parameters {
    *  done
    */
 
-  // The start of round 0 is set to 2017-01-17 19:00 of timezone Europe/Zurich
+  // The start of round 0 is set to 2017-01-24 19:00 of timezone Europe/Zurich
   uint public constant round0StartTime      = 1484676000 + 1 weeks; 
   
   // The start of round 1 is set to 2017-05-17 19:00 of timezone Europe/Zurich
